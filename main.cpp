2130f5a0189e940257823e18eed233ffa11d0dc8
Dov Djian <dovdjianpro@gmail.com>
2021-12-25 23:59:59 +0100

    [+] init all the prototype, start of test vector

--- main.cpp
+++ main.cpp
@@ -6,34 +6,28 @@
 /*   By: dodjian <dovdjianpro@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 20:28:18 by dodjian           #+#    #+#             */
-/*   Updated: 2021/12/25 01:39:38 by dodjian          ###   ########.fr       */
+/*   Updated: 2021/12/25 23:59:15 by dodjian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Vector.hpp"
+#include "vector.hpp"
 
 int main(void)
 {
-	std::cout << GREEN << "std::vector |" << RED << " ft::vector" << std::endl << std::endl;
-	std::vector<int> std_vector;
-	ft::vector<int> my_vector;
-	for (int i = 0; i < 5; i++)
-		std_vector.push_back(i);
-	//std::cout << "std::vector contains: ";
-	//for (std::vector<int>::iterator it = std_vector.begin() ; it != std_vector.end(); ++it)
-		//std::cout << GREEN << *it << ' ';
-	//std::cout << std::endl << END;
+	//std::cout << BGREEN << "C++ |" << BRED << " MOI" << END << std::endl << std::endl;
+	std::cout << BRED << "Create obj with all constructor !" << END << std::endl << std::endl;
 
-	/* ft::vector<int> my_vector;
-	for (int i = 0; i < 5; i++)
-		my_vector.push_back(i);
-	std::cout << "my_vector contains:";
-	for (iterator it = my_vector.begin() ; it != my_vector.end(); ++it)
-		std::cout << RED << *it << ' ';
-	std::cout << std::endl << END; */
+	std::vector<int> first;                                // empty vector of ints
+	std::vector<int> second (4,100);                       // four ints with value 100
+	std::vector<int> third (second.begin(),second.end());  // iterating through second
+	std::vector<int> fourth (third);
+	// the iterator constructor can also be used to construct from arrays:
+	int myints[] = {16,2,77,29};
+	std::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int));
 
-
-	std::cout << GREEN << "std::vector::empty = " << std_vector.empty() << std::endl;
-	std::cout << RED << "ft::vector::empty = " << my_vector.empty() << std::endl << END;
+	std::cout << BYELLOW << "The contents of fifth are:" << BBLUE;
+	for (std::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
 	return (0);
 }
